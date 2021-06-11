ffbeb95584d06e875143eaeda7ae2fbeb66f0474
pfpah482 <60918046+pfpah482@users.noreply.github.com>
2021-06-11 13:22:41 +0300

    Update flask.py

--- flask.py
+++ flask.py
@@ -7,9 +7,9 @@ from flask import *
 from ndvi import *
 
 if (__name__=='__main__'):
-	app = Flask(__name__)
-	app.config['TEMPLATES_AUTO_RELOAD'] = True
-	app.config['UPLOAD_EXTENSIONS'] = ['.tif']
+	flask_application = Flask(__name__)
+	flask_application.config['TEMPLATES_AUTO_RELOAD'] = True
+	flask_application.config['UPLOAD_EXTENSIONS'] = ['.tif']
 
 @app.route('/')
 def index():
@@ -42,16 +42,18 @@ def upload_files():
 			else:
 				nir_image = Image.open(uploaded_file.stream)
 
+    	nir_image = np.array(nir_image, dtype='uint8')
+    	red_image = np.array(red_image, dtype='uint8')
 	if red_image == None or nir_image == None:
-		 abort(400)
+		abort(400)
 
 	ndvi = get_ndvi(nir_image, red_image)
 	greyscale = ((ndvi + 1.)/2.) * 256.
-    heatmap = cv2.cvtColor(cv2.applyColorMap(greyscale.astype('uint8'), cv2.COLORMAP_SUMMER), cv2.COLOR_BGR2RGB)
-    
+    	heatmap = cv2.cvtColor(cv2.applyColorMap(greyscale.astype('uint8'), cv2.COLORMAP_SUMMER), cv2.COLOR_BGR2RGB)
+	
 	uri = get_uri(heatmap)
 
 	return render_template('result.html', image=uri)
 
 if (__name__=='__main__'):
-	app.run()
+	flask_application.run()
